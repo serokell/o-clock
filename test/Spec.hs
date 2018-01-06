c4ef2bd15c924b2fb5d152557a2488f6ddc839e6
Dmitry Kovanikov <kovanikov@gmail.com>
2018-01-06 03:59:36 +0300

    Add type families tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
+module Main where
+
+import Test.Time.TypeSpec (typeSpec_DivRat, typeSpec_Gcd, typeSpec_Normalize,
+                           typeSpec_UnitCalculation)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    -- type specs
+    print typeSpec_Gcd
+    print typeSpec_Normalize
+    print typeSpec_DivRat
+    print typeSpec_UnitCalculation
