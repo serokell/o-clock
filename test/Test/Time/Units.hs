004bae473a8ad5d0b72f3b47b3572eab8607f1b7
vrom911 <vrom911@gmail.com>
2018-01-07 23:02:13 +0300

    [#6] Refactor tests to use creation helpers

--- test/Test/Time/Units.hs
+++ test/Test/Time/Units.hs
@@ -11,8 +11,8 @@ import Data.Ratio ((%))
 import Test.Tasty (TestTree, defaultMain)
 import Test.Tasty.Hspec (Spec, describe, it, shouldBe, testSpec)
 
-import Time (Day, DayUnit, Fortnight, MicroSecond, MicroSecondUnit, MilliSecond, MilliSecondUnit,
-             Second, SecondUnit, Time (..), WeekUnit, convertUnit)
+import Time (DayUnit, MicroSecondUnit, MilliSecondUnit, SecondUnit, Time (..), WeekUnit,
+             convertUnit, day, fortnight, mcs, ms, sec)
 
 runTests :: IO ()
 runTests = do
@@ -26,14 +26,14 @@ spec_convertUnit :: Spec
 spec_convertUnit =
     describe "Unit Conversion Test" $ do
         it "11 seconds is 11000 milliseconds" $
-            convertUnit @MilliSecondUnit (11 :: Second) `shouldBe` 11000
+            convertUnit @MilliSecondUnit (sec 11) `shouldBe` 11000
         it "5000 milliseconds is 5 seconds" $
-            convertUnit @SecondUnit (5000 :: MilliSecond) `shouldBe` 5
+            convertUnit @SecondUnit (ms 5000) `shouldBe` 5
         it "3 seconds is 3000000 microseconds" $
-            convertUnit @MicroSecondUnit (3 :: Second) `shouldBe` 3000000
+            convertUnit @MicroSecondUnit (sec 3) `shouldBe` 3000000
         it "3 microseconds is 3/1000000 seconds" $
-            convertUnit @SecondUnit (3 :: MicroSecond) `shouldBe` Time (3 % 1000000)
+            convertUnit @SecondUnit (mcs 3) `shouldBe` Time (3 % 1000000)
         it "7 days is 1 week" $
-            convertUnit @WeekUnit (7 :: Day) `shouldBe` 1
+            convertUnit @WeekUnit (day 7) `shouldBe` 1
         it "2 fornights is 28 days" $
-            convertUnit @DayUnit (2 :: Fortnight) `shouldBe` 28
+            convertUnit @DayUnit (fortnight 2) `shouldBe` 28
