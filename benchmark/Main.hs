87418dfc4809d984781b4067090dd6220241efad
vrom911 <vrom911@gmail.com>
2018-01-14 02:41:13 +0300

    [#33] Add benchmarks with gauge

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -1,2 +1,25 @@
+{-# LANGUAGE TypeApplications #-}
+
+module Main where
+
+import Gauge (bench, bgroup, defaultMain, whnf)
+
+import Time (Day, Nanosecond, Second, ns, sec, toUnit, week)
+
+import qualified Data.Time.Units as TU (Day, Nanosecond, Second, Week, convertUnit)
+
 main :: IO ()
-main = undefined
+main = defaultMain
+    [ bgroup "Second to Nanosecond"
+        [ bench "o'clock"    $ whnf (toUnit @Nanosecond . sec) 1
+        , bench "time-units" $ whnf (TU.convertUnit :: TU.Second -> TU.Nanosecond) 1
+        ]
+    , bgroup "1000ns to s"
+        [ bench "o'clock"    $ whnf (toUnit @Second . ns) 1000
+        , bench "time-units" $ whnf (TU.convertUnit :: TU.Nanosecond -> TU.Second) 1000
+        ]
+    , bgroup "week to days"
+        [ bench "o'clock"    $ whnf (toUnit @Day . week ) 1
+        , bench "time-units" $ whnf (TU.convertUnit :: TU.Week -> TU.Day) 1
+        ]
+    ]
