f0dd727ee99632b7099528a61a50af74568d673c
vrom911 <vrom911@gmail.com>
2018-01-17 18:08:04 +0300

    [#33] Use nf in o'clock benchs, add Generic instance to Time

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -1,25 +1,32 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+{-# LANGUAGE DataKinds        #-}
+{-# LANGUAGE KindSignatures   #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Main where
 
-import Gauge (bench, bgroup, defaultMain, whnf)
+import Control.DeepSeq (NFData)
+import Gauge (bench, bgroup, defaultMain, nf, whnf)
 
-import Time (Day, Nanosecond, Second, ns, sec, toUnit, week)
+import Time (Day, Nanosecond, Rat, Second, Time, ns, sec, toUnit, week)
 
 import qualified Data.Time.Units as TU (Day, Nanosecond, Second, Week, convertUnit)
+
+instance NFData (Time (unit :: Rat))
 
 main :: IO ()
 main = defaultMain
     [ bgroup "Second to Nanosecond"
-        [ bench "o'clock"    $ whnf (toUnit @Nanosecond . sec) 1
+        [ bench "o'clock"    $ nf (toUnit @Nanosecond . sec) 1
         , bench "time-units" $ whnf (TU.convertUnit :: TU.Second -> TU.Nanosecond) 1
         ]
     , bgroup "1000ns to s"
-        [ bench "o'clock"    $ whnf (toUnit @Second . ns) 1000
+        [ bench "o'clock"    $ nf (toUnit @Second . ns) 1000
         , bench "time-units" $ whnf (TU.convertUnit :: TU.Nanosecond -> TU.Second) 1000
         ]
     , bgroup "week to days"
-        [ bench "o'clock"    $ whnf (toUnit @Day . week ) 1
+        [ bench "o'clock"    $ nf (toUnit @Day . week ) 1
         , bench "time-units" $ whnf (TU.convertUnit :: TU.Week -> TU.Day) 1
         ]
     ]
